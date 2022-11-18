5b913025265a6be677962d6dae81026b4acb4d01
abiiralbhattarai <bhattarai.abi07@gmail.com>
2022-11-18 17:46:43 +0545

    Custom module practise

---
+++ Beginner Folder/3-modulePractise.js
@@ -0,0 +1,8 @@
+// Modules - Enacapsulated Code (only share minimum)
+
+const names = require("./4-modulesNames");
+const sayHi = require("./5-utils");
+
+sayHi("susan")
+sayHi(names.john)
+sayHi(names.peter)
