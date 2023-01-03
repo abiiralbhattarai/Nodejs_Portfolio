48b6e48ae8d8753e65d2649f8bbb4f3b1d170eb3
abiiralbhattarai <abiiralbhattarai@gmail.com>
2023-01-03 10:09:42 +0545

    post request using async await

--- RestApi/src/app.js
+++ RestApi/src/app.js
@@ -14,18 +14,32 @@ app.get("/students", (req, res) => {
   res.send("Hello from other sides.");
 });
 //create a new students
-app.post("/students", (req, res) => {
+/*app.post("/students", (req, res) => {
   console.log(req.body);
   const user = new Student(req.body);
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
     })
     .catch((e) => {
-      res.send(e);
+      res.status(400).send(e);
     });
+});*/
+
+//using async await
+app.post("/students", async (req, res) => {
+  console.log(req.body);
+
+  try {
+    const user = new Student(req.body);
+    const createUser = await user.save();
+    res.status(201).send(createUser);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
+
 app.listen(port, () => {
   console.log(`connection is setup at ${port}`);
 });
