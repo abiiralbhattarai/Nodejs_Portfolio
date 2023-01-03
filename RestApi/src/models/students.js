76d99b196e26e6ee484378e423caac7af5755d64
abiiralbhattarai <abiiralbhattarai@gmail.com>
2023-01-03 09:39:16 +0545

    Student Model

---
+++ RestApi/src/models/students.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minimumlength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: [true, "Email id already present"],
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid Email");
+      }
+    },
+  },
+  phone: {
+    type: Number,
+    min: 10,
+    max: 10,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+//creating new collection
+
+const Student = new mongoose.model('Student',studentSchema);
+
+module.exports = Student;
