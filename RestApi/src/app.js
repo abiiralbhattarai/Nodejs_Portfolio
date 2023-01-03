a9a5d336668312a7a63ea3090fd06740e15097a2
abiiralbhattarai <abiiralbhattarai@gmail.com>
2023-01-03 08:24:19 +0545

    Databse connected

--- RestApi/src/app.js
+++ RestApi/src/app.js
@@ -1,6 +1,18 @@
 const express = require("express");
+require("./db/conn");
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.get("/students",(req,res) => {
-    
-})
+app.get("/", (req, res) => {
+  res.send("Hello I am Abiral");
+});
+app.get("/students", (req, res) => {
+  res.send("Hello from other sides.");
+});
+//create a new students
+app.post("/students", (req, res) => {
+  res.send("Hello");
+});
+app.listen(port, () => {
+  console.log(`connection is setup at ${port}`);
+});
